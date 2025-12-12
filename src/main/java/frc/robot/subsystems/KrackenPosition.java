72a28a962b62a864121027b5280e5fdba0ff0721
SLUH Robotics <robotics@sluh.org>
2025-12-12 11:43:41 -0600

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/KrackenPosition.java
@@ -0,0 +1,53 @@
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.*;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+public class KrackenPosition {
+    private final TalonFX m_posMotorFx = new TalonFX(0);
+
+    private final PositionVoltage m_posVoltage = new PositionVoltage(0);
+
+    private double motorRotations = 0;
+
+    public KrackenPosition() {
+        m_posMotorFx.getConfigurator().apply(new TalonFXConfiguration());
+
+        /* Create and apply configuation for angle motor 
+        ****** Still need to set PID values ******
+        */
+        TalonFXConfiguration m_posMotorConfig = new TalonFXConfiguration();
+        m_posMotorConfig.Slot0.kP = 0; // An erro of 1 rotation results in a 0 V output
+        m_posMotorConfig.Slot0.kI = 0; // No output for integrate error
+        m_posMotorConfig.Slot0.kD = 0; // A velocity of 1 rotation results in a 0 V output
+        // Peak output of 8 V
+        m_posMotorConfig.Voltage.withPeakForwardVoltage(Volts.of(8))
+        .withPeakReverseVoltage(Volts.of(-8));
+        m_posMotorFx.setNeutralMode(NeutralModeValue.Brake);
+
+        /* Retry config apply up to 5 times, report if failure */
+        StatusCode status = StatusCode.StatusCodeNotInitialized;
+        for (int i = 0; i < 5; ++i) {
+        status = m_posMotorFx.getConfigurator().apply(m_posMotorConfig);
+        if (status.isOK()) break;
+        }
+        if (!status.isOK()) {
+            System.out.println("Could not apply configs to DeepClimb motor ONE, error code: " + status.toString());
+        }
+
+        /* Make sure we start at 0 */
+        m_posMotorFx.setPosition(0);
+    }   
+
+    public void setPosition(double positionInterval) {
+        motorRotations += positionInterval;
+        // ADD SOFT STOP FOR SAFETY
+
+        m_posMotorFx.setControl(m_posVoltage.withPosition(motorRotations));
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/NeoSpeed.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+public class NeoSpeed {
+    
+    private final SparkMax m_rollingMotor = new SparkMax(1, MotorType.kBrushless);
+
+    public NeoSpeed() {
+        
+        m_rollingMotor.setCANTimeout(300);
+
+    SparkMaxConfig rollerConfig = new SparkMaxConfig();
+    rollerConfig.voltageCompensation(10);
+    rollerConfig.smartCurrentLimit(60);
+    m_rollingMotor.configure(rollerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    public void runRoller(double speed) {
+        m_rollingMotor.set(speed);
+    }
+}
+
